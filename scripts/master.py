ba9dda312df091184e38d9e7f4878f201b10e39d
Your Name <you@example.com>
2019-02-13 19:05:47 -0800

    update

--- scripts/master.py
+++ scripts/master.py
@@ -7,10 +7,12 @@ import random
 
 from std_msgs.msg import Float32
 from std_msgs.msg import Int32
+from std_msgs.msg import Empty as EmptyMsg
 from SyncTemplate.msg import AckMsg
 
 rospy.init_node('Master', anonymous=True)
 sync_flag_pub =rospy.Publisher('/sync_flag', Int32, queue_size=100)
+outer_loop_pub =rospy.Publisher('/outer_loop', EmptyMsg, queue_size=1)
 
 class Master:
 	def __init__(self):
@@ -25,13 +27,15 @@ class Master:
 
             for s in self.slaves.keys():
                 rospy.Subscriber(s+'/ack', AckMsg, self.sentCB)
-            time.sleep(3)
-            self.check_sum=None
+            time.sleep(.5)
+            self.check_sum=Int32()
+            self.check_sum.data=0
             self.send_flag()
 
         def sentCB(self,msg):
             try:
-                self.slaves[msg.id]=True
+                if msg.check_sum==self.check_sum.data:
+                  self.slaves[msg.id]=True
             except Exception,e: print str(e)
             if self.check_all_sent():
                 self.send_flag()
@@ -52,17 +56,29 @@ class Master:
 
             # for k,v in self.slaves.items():
             #   print k,v,'after'
-            self.check_sum=Int32()
-            self.check_sum.data=random.randint(0,10000)
-            self.pub()
+            # self.check_sum=Int32()
+            # self.check_sum.data=random.randint(0,10000)
+            self.check_sum.data+=1
+            # self.pub()
 
         def pub(self):
             sync_flag_pub.publish(self.check_sum)
 
+        def outer_loop_inc(self):
+          # self.check_sum.data=1
+          outer_loop_pub.publish(EmptyMsg())
+          time.sleep(2)
+
         def run(self):
+            now=time.time()
             while not rospy.is_shutdown():
-                self.pub()
-                time.sleep(1)
+              if time.time()-now>3:
+                time.sleep(.25)
+                now=time.time()
+                self.outer_loop_inc()
+
+              self.pub()
+              time.sleep(.001)
 
 def main(args):
 	manager=Master()
--- scripts/slave.py
+++ scripts/slave.py
@@ -5,6 +5,7 @@ import time
 import rospy
 import sys
 from std_msgs.msg import Float32
+from std_msgs.msg import Empty as EmptyMsg
 from std_msgs.msg import Int32
 from SyncTemplate.msg import TwoScalarMsg
 from SyncTemplate.msg import AckMsg
@@ -14,8 +15,10 @@ rospy.init_node('Slave', anonymous=True)
 val=[]
 line_iter=0
 
-if rospy.has_param("~master"):
-    master = rospy.get_param("~master")
+# if rospy.has_param("~master"):
+#     master = rospy.get_param("~master")
+# else:
+#     master=False
 
 
 
@@ -26,6 +29,7 @@ class Slave:
             else:
                 print "Identity of this computer is not set. Exiting"
                 exit()
+            self.f=open('/home/aaron/catkin_ws/src/SyncTemplate/scripts/'+str(self.ident)+".csv","w")
 
             if self.ident==1:
                 with open('/home/aaron/catkin_ws/src/ratio_consensus/scripts/scaled_interp_Pref.csv') as csv_file:
@@ -52,7 +56,7 @@ class Slave:
             for i in range(100):
                 try:
                     s=rospy.get_param("~incoming"+str(i))
-                    self.incoming_neighbors[s] = [False,TwoScalarMsg()]
+                    self.incoming_neighbors[s] = {}
                     print "Agent", self.ident, "now listening to", s
                 except:
                     pass
@@ -60,9 +64,12 @@ class Slave:
             self.check_sum=None
             self.ack_publisher =rospy.Publisher('ack', AckMsg, queue_size=1)
             rospy.Subscriber('/sync_flag', Int32, self.flagCB)
+            rospy.Subscriber('/outer_loop', EmptyMsg, self.outerLoopCB)
             for s in self.incoming_neighbors.keys():
                 rospy.Subscriber('/'+s+'/edge', TwoScalarMsg, self.neighborCB)
+            self.iteration2=0
             self.check_list=[0]
+            self.validated={}
             self.val_list={}
             self.val_list[0]=TwoScalarMsg()
             self.val_list[0].val0=0
@@ -95,10 +102,26 @@ class Slave:
           v1.id=v2.id
           return v1
 
+        def outerLoopCB(self,msg):
+            self.f.write(str(self.gu+(self.val_list[self.check_list[0]].val1/self.val_list[self.check_list[0]].val2)*(self.go-self.gu))+"\n")
+            # time.sleep(1)
+            if self.ident==1:
+              time.sleep(.25)
+              self.iteration2+=1
+              self.update_y()
+
+                # if self.ident==1:
+                #   self.iteration2+=1
+                #   self.update_y()
+
+
         def flagCB(self,msg):
             # print "Agent",self.ident,"receiving round flag"
             if msg.data in self.check_list:
-              return
+              if msg.data==self.check_list[1]:
+                self.pub()
+                return
+            self.check_sum=msg.data
             self.check_list.append(msg.data)
             self.val_list[msg.data]=TwoScalarMsg()
             self.val_list[msg.data].val0=msg.data
@@ -106,10 +129,9 @@ class Slave:
             if len(self.val_list)>2:
               del self.val_list[self.check_list[0]]
               del self.check_list[0]
-            print "Starting new", msg.data, self.ident
-            for v in self.incoming_neighbors.values():
-              v[0]=False
-            self.check_sum=msg.data
+            # print "Starting new", msg.data, self.ident
+            # for v in self.incoming_neighbors.values():
+            #   v[0]=False
             # self.val_msg.val0=self.check_sum
             self.update_flag=True
             # print self.check_sum
@@ -126,30 +148,110 @@ class Slave:
 
         def pub(self):
           if len(self.val_list)<1:
+            raise Exception('Small list error')
             return
+          elif len(self.val_list)==2:
+            # if self.check_sum==self.check_list[0]:
+            #   raise Exception('firstcheck(SUM',self.check_sum,self.check_list,self.ident)
+            if self.check_sum!=self.check_list[1]:
+              # self.pub()
+              # return
+              raise Exception('check(SUM',self.check_sum,self.check_list,self.ident)
+              return
+
           # print self.val_msg
-          print self.val_list[self.check_list[0]]
+          print self.val_list[self.check_list[0]],self.check_list[0]
           self.outgoing_pub.publish(self.val_list[self.check_list[0]])
-            # self.send_ack()
+          # if self.update_flag==False:
+          #   self.send_ack()
             # self.val_msg_last=self.copy_val_msg(self.val_msg_last,self.val_msg)
 
 
         def neighborCB(self,msg):
             # print "Agent",self.ident,"received msg",msg
-            try:
-                self.incoming_neighbors[msg.id][0]=True
-                self.incoming_neighbors[msg.id][1].val0=msg.val0
-                self.incoming_neighbors[msg.id][1].val1=msg.val1
-                self.incoming_neighbors[msg.id][1].val2=msg.val2
-            except Exception,e: print str(e)
-            if self.check_all_sent_from_neighbors():
+            if msg.val0 != self.check_list[0]:
+              return
+            if self.incoming_neighbors[msg.id].get(msg.val0) is None:
+              # try:
+              self.incoming_neighbors[msg.id][msg.val0]={}
+              self.incoming_neighbors[msg.id][msg.val0][1]=msg.val1
+              self.incoming_neighbors[msg.id][msg.val0][2]=msg.val2
+              # except:
+              #     self.incoming_neighbors[msg.id][msg.val0]=None
+              #     return
+
+              # self.incoming_neighbors[msg.id][msg.val0].val0=msg.val0
+              # self.incoming_neighbors[msg.id][msg.val0].val1=msg.val1
+              # self.incoming_neighbors[msg.id][msg.val0].val2=msg.val2
+            else:
+              return
+
+
+            # if self.update_flag==False:
+            #   return False
+            if len(self.val_list)<2:
+              raise Exception('I know Python!')
+              return False
+
+            for k,v in self.incoming_neighbors.items():
+                if v.get(self.check_list[0]) is None:
+                  return False
+
+                
+#                 # print k,v
+#                 if v[1].val0 != self.check_list[0]:
+#                     return False
+
+            # self.update_flag=False
+            z_sum=0.0
+            y_sum=0.0
+            for k,yz in self.incoming_neighbors.items():
+                # print k,yz
+                # try:
+                y_sum+=yz[self.check_list[0]][1]
+                z_sum+=yz[self.check_list[0]][2]
+                # except:
+                #   del self.incoming_neighbors[k][self.check_list[0]]
+                #   return
+                  # raise Exception(yz,"ERROR",self.ident,self.check_list[0])
+            y=1./3.*(self.val_list[self.check_list[0]].val1+y_sum)
+            z=1./3.*(self.val_list[self.check_list[0]].val2+z_sum)
+            # time.time(1)
+
+            self.val_list[self.check_list[1]].val1=y
+            self.val_list[self.check_list[1]].val2=z
+
+            # self.yz_msg=Quaternion()
+            # self.yz_msg.y=y
+            # self.yz_msg.z=z
+            # self.yz_msg.x=int(self.iteration+1)
+            # self.yz_msg.w=int(ident)
+            # self.iteration+=1
+            # self.pub()
+            '''
+            All values received from neighbors.  some computation probably goes here and then an acknowledgement is sent to the synchronizer.
+            '''
+            # print "Updated val", self.ident
+            self.send_ack()
+            # return True
+
+
+
+
+
+
+            # if self.check_all_sent_from_neighbors():
                 # pass
-                self.send_ack()
+                # self.send_ack()
 
         def check_all_sent_from_neighbors(self):
             if self.update_flag==False:
               return False
+            if len(self.val_list)<2:
+              raise Exception('I know Python!')
+              return False
             for k,v in self.incoming_neighbors.items():
+              # if self.validated[v.val0][k]:
                 # print k,v
                 if v[0] == False or v[1].val0 != self.check_list[0]:
                     return False
@@ -178,7 +280,7 @@ class Slave:
             '''
             All values received from neighbors.  some computation probably goes here and then an acknowledgement is sent to the synchronizer.
             '''
-            print "Updated val", self.ident
+            # print "Updated val", self.ident
             return True
 
         def send_ack(self):
@@ -193,10 +295,21 @@ class Slave:
             # print ack_response,'ACK', type(ack_response.id)
             self.ack_publisher.publish(ack_response)
 
+        def update_y(self):
+          print self.val_list[self.check_list[0]],self.iteration2
+          for k,v in self.val_list.items():
+            self.val_list[k].val1+=float(val[self.iteration2])-float(val[self.iteration2-1])
+
         def run(self):
+            # f=open('/home/aaron/catkin_ws/src/SyncTemplate/scripts/'+str(self.ident)+".csv","w")
             while not rospy.is_shutdown():
-                self.pub()
-                time.sleep(.01)
+                # self.pub()
+                time.sleep(1)
+                # f.write(str(self.gu+(self.val_list[self.check_list[0]].val1/self.val_list[self.check_list[0]].val2)*(self.go-self.gu))+"\n")
+                # time.sleep(3)
+                # if self.ident==1:
+                #   self.iteration2+=1
+                #   self.update_y()
 
 def main(args):
 	manager=Slave()
